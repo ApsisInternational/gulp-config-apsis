3c8a605fc1746fcb29e831cb512dd0189644fdd9
Hampus Persson <h@hmps.se>
2016-04-08 15:59:44 +0200

    feat: setup changelog with new template

--- src/tasks/release.js
+++ src/tasks/release.js
@@ -7,6 +7,24 @@ import gutil from 'gulp-util';
 import conventionalRecommendedBump from 'conventional-recommended-bump';
 import conventionalChangelog from 'gulp-conventional-changelog';
 
+const headerPartial = '{{#if isPatch}}##{{else}}#{{/if}} [{{version}}](http://tfs/tfs/Apsis/JSPM/_git/wizard#path=%2F&version=GTv{{version}}){{#if title}} "{{title}}"{{/if}}{{#if date}} ({{date}}){{/if}}';
+const commitPartial = `* {{header}} {{#if @root.linkReferences}}([{{shortHash}}]({{#if @root.host}}{{@root.host}}/{{/if}}{{#if @root.owner}}{{@root.owner}}/{{/if}}{{@root.repository}}/{{@root.commit}}/{{hash}})){{else}}{{hash~}}{{/if}} {{~!-- commit references --}}{{#if references}}, closes{{~#each references}} {{#if @root.linkReferences}}[{{#if this.owner}}{{this.owner}}/{{/if}}{{this.repository}}#{{this.issue}}]({{#if @root.host}}{{@root.host}}/{{/if}}{{#if this.repository}}{{#if this.owner}}{{this.owner}}/{{/if}}{{this.repository}}{{else}}{{#if @root.owner}}{{@root.owner}}/{{/if}}{{@root.repository}}{{/if}}/{{@root.issue}}/{{this.issue}}){{else}}{{#if this.owner}}{{this.owner}}/{{/if}}{{this.repository}}#{{this.issue}}{{/if}}{{/each}}{{/if}}\n`;
+const mainTemplate = `{{> header}}
+
+{{#each commitGroups}}
+
+{{#if title}}
+### {{title}}
+
+{{/if}}
+{{#each commits}}
+{{> commit root=@root}}
+{{/each}}
+{{/each}}
+
+{{> footer}}
+
+`;
 
 export function releaseTasks(gulp) {
     taskMaker(gulp)
@@ -80,6 +98,45 @@ export function releaseTasks(gulp) {
             .pipe(conventionalChangelog({
                 preset: 'angular',
                 releaseCount: 0,
+            }, {
+                commit: 'commit',
+            }, {}, {}, {
+                mainTemplate,
+                headerPartial,
+                commitPartial,
+                transform: function transform(commit) {
+                    let discard = true;
+                    commit.shortHash = commit.hash.substring(0, 7);
+
+                    commit.notes.forEach(note => {
+                        note.title = 'BREAKING CHANGES';
+                        discard = false;
+                    });
+
+                    if (commit.type === 'feat') {
+                        commit.type = 'Features';
+                    } else if (commit.type === 'fix') {
+                        commit.type = 'Bug Fixes';
+                    } else if (commit.type === 'perf') {
+                        commit.type = 'Performance Improvements';
+                    } else if (commit.type === 'revert') {
+                        commit.type = 'Reverts';
+                    } else if (discard) {
+                        return false;
+                    } else if (commit.type === 'docs') {
+                        commit.type = 'Documentation';
+                    } else if (commit.type === 'style') {
+                        commit.type = 'Styles';
+                    } else if (commit.type === 'refactor') {
+                        commit.type = 'Code Refactoring';
+                    } else if (commit.type === 'test') {
+                        commit.type = 'Tests';
+                    } else if (commit.type === 'chore') {
+                        commit.type = 'Chores';
+                    }
+
+                    return commit;
+                }
             }))
             .pipe(gulp.dest('./'));
     }
